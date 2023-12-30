5d0198511697cb74e03d46ead8ea9bf8d8e48d7e
miguelcsilva <66789107+miguelcsilva@users.noreply.github.com>
2023-12-30 16:22:46 +0000

    Fix formatting and linting (#2)
    * Fix formatting and linting
    
    * Rename ci job

--- src/file.rs
+++ src/file.rs
@@ -5,6 +5,9 @@ use std::path::PathBuf;
 pub fn get_files(directory: &str) -> Vec<PathBuf> {
     let search_pattern = directory.to_string() + "/**/[!.]*";
     let paths = glob(&search_pattern).unwrap();
-    let file_paths: Vec<PathBuf> = paths.map(|x| x.unwrap()).filter(|file| file.is_file()).collect();
+    let file_paths: Vec<PathBuf> = paths
+        .map(|x| x.unwrap())
+        .filter(|file| file.is_file())
+        .collect();
     file_paths
 }
